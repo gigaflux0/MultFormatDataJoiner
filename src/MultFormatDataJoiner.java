5363bfe5055fade3644d99c297fe6544616461e9
George Mason <georgemason321@hotmail.com>
2017-08-27 20:54:42 +0100

    Lay code architecture, add json and csv open libraries

--- src/MultFormatDataJoiner.java
+++ src/MultFormatDataJoiner.java
@@ -1,9 +1,49 @@
+import java.util.LinkedList;
+import java.util.List;
 
 public class MultFormatDataJoiner {
+	
+	private static class DataCompiler {
+		List<Data> data;
+		
+		DataCompiler() {
+			data = new LinkedList<>();
+		}
+		
+		public void add(String path) {
+			//
+		}
+		
+/*		
+		private void add(//CSV) {
+			//
+		}
+		
+		private void add(//JSON) {
+			//
+		}
+		
+		private void add(//XML) {
+			//
+		}
+*/
+		
+		public void write(String path) {
+			//
+		}
+		
+		private class Data {
+			//
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		DataCompiler data = new DataCompiler();
+		data.add("Data/users.csv");
+		data.add("Data/users.json");
+		data.add("Data/users.xml");
+		//folder name for output data
+		data.write("Result");
 	}
 
 }
