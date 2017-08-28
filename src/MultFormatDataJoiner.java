c397aea647ad54e0cb75ae2cab6dc8f58cd0f83a
George Mason <georgemason321@hotmail.com>
2017-08-28 14:30:16 +0100

    Add XML parser and linkedlist quicksorter

--- src/MultFormatDataJoiner.java
+++ src/MultFormatDataJoiner.java
@@ -1,8 +1,19 @@
+import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import com.Ostermiller.util.CSVParser;
 import com.Ostermiller.util.LabeledCSVParser;
@@ -40,7 +51,10 @@ public class MultFormatDataJoiner {
 					add(g, path);
 				}
 				else {
-					System.out.println("oh aye3");
+					DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+					try {
+						add(dbFactory, path);
+					} catch (Exception e) {e.printStackTrace();}
 				}
 			}
 			else
@@ -75,11 +89,34 @@ public class MultFormatDataJoiner {
 				dataList.addAll(proData);
 			} catch (IOException e) {e.printStackTrace();}
 		}
-/*		
-		private void add(//XML) {
-			//
+
+		private void add(DocumentBuilderFactory dbFactory, String path) {
+			try {				
+				File fXmlFile = new File(path);
+				DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+				Document doc = dBuilder.parse(fXmlFile);
+				doc.getDocumentElement().normalize();
+				NodeList nList = doc.getElementsByTagName("user");
+				for (int temp = 0; temp < nList.getLength(); temp++) {
+					Node nNode = nList.item(temp);
+					if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+						Element eElement = (Element) nNode;
+						Data data = new Data(eElement.getElementsByTagName("userid").item(0).getTextContent(), 
+											 eElement.getElementsByTagName("firstname").item(0).getTextContent(), 
+											 eElement.getElementsByTagName("surname").item(0).getTextContent(), 
+											 eElement.getElementsByTagName("username").item(0).getTextContent(), 
+											 eElement.getElementsByTagName("type").item(0).getTextContent(), 
+											 eElement.getElementsByTagName("lastlogintime").item(0).getTextContent());
+						dataList.add(data);
+					}
+				}
+				
+			} catch(Exception e) {e.printStackTrace();}
 		}
-*/
+
+		public void sort() {
+			Collections.sort(dataList, Comparator.comparingInt(Data::getId));
+		}
 		
 		public void write(String path) {
 			//
@@ -109,6 +146,10 @@ public class MultFormatDataJoiner {
 				this.last_login_time = lastLoginTime;
 			}
 			
+			public int getId() {
+				return user_id;
+			}
+			
 			public String toString() {
 				return (Integer.toString(user_id)+"   "+first_name+"   "+last_name+
 						"   "+username+"   "+user_type+"   "+last_login_time);
@@ -121,6 +162,7 @@ public class MultFormatDataJoiner {
 		data.add("Data/users.csv");
 		data.add("Data/users.json");
 		data.add("Data/users.xml");
+		data.sort();
 		data.printDataList();
 		//folder name for output data
 		data.write("Result");
