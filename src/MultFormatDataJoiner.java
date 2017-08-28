85a7efe8dfdb67734a972591c3678a525640ebfc
George Mason <georgemason321@hotmail.com>
2017-08-28 16:36:44 +0100

    Add autosort switch, csv writer, nested writer class

--- src/MultFormatDataJoiner.java
+++ src/MultFormatDataJoiner.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
@@ -16,6 +17,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import com.Ostermiller.util.CSVParser;
+import com.Ostermiller.util.CSVPrinter;
 import com.Ostermiller.util.LabeledCSVParser;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -27,10 +29,17 @@ import java.nio.file.Paths;
 public class MultFormatDataJoiner {
 	
 	private static class DataCompiler {
-		List<Data> dataList;
+		private List<Data> dataList;
+		private boolean autosort;
 		
 		DataCompiler() {
 			dataList = new LinkedList<>();
+			autosort = false;
+		}
+		
+		//Disabled by default, if enabled data sorts each time more is added
+		public void autoSort(boolean in) {
+			this.autosort = in;
 		}
 		
 		public void add(String path) {
@@ -73,6 +82,7 @@ public class MultFormatDataJoiner {
 										 lcsvp.getValueByLabel("Last Login Time"));
 					dataList.add(data);
 				}
+				if(autosort) sort();
 			} catch (IllegalStateException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
@@ -87,6 +97,7 @@ public class MultFormatDataJoiner {
 				@SuppressWarnings("unchecked")
 				List<Data> proData = (LinkedList<Data>) g.fromJson(jsonFile, listType);
 				dataList.addAll(proData);
+				if(autosort) sort();
 			} catch (IOException e) {e.printStackTrace();}
 		}
 
@@ -110,7 +121,7 @@ public class MultFormatDataJoiner {
 						dataList.add(data);
 					}
 				}
-				
+				if(autosort) sort();
 			} catch(Exception e) {e.printStackTrace();}
 		}
 
@@ -119,7 +130,44 @@ public class MultFormatDataJoiner {
 		}
 		
 		public void write(String path) {
-			//
+			//Nesting writer methods in inner class to keep code clean
+			new Writer(path);
+		}
+		
+		private class Writer {
+			private String path;
+			
+			Writer(String path) {
+				this.path = path;
+				writeCsv();
+				writeJson();
+				writeXML();
+			}
+			
+			private void writeCsv() {
+				try{
+				    PrintWriter writer = new PrintWriter(path+"/users.csv", "UTF-8");
+				    CSVPrinter csvp = new CSVPrinter(writer);
+				    csvp.writeln(new String[]{"User ID","First Name","Last Name","Username","User Type","Last Login Time"});
+				    for(Data data : dataList) {
+				    	csvp.writeln(new String[]{Integer.toString(data.getId()),
+				    							  data.getFirstName(),
+				    							  data.getLastName(),
+				    							  data.getUsername(),
+				    							  data.getUserType(),
+				    							  data.getLastLoginTime()});
+				    }
+				    writer.close();
+				} catch (Exception e) {e.printStackTrace();}
+			}
+			
+			private void writeJson() {
+				//
+			}
+			
+			private void writeXML() {
+				//
+			}
 		}
 		
 		public void printDataList() {
@@ -150,6 +198,26 @@ public class MultFormatDataJoiner {
 				return user_id;
 			}
 			
+			public String getFirstName() {
+				return first_name;
+			}
+			
+			public String getLastName() {
+				return last_name;
+			}
+			
+			public String getUsername() {
+				return username;
+			}
+			
+			public String getUserType() {
+				return user_type;
+			}
+			
+			public String getLastLoginTime() {
+				return last_login_time;
+			}
+			
 			public String toString() {
 				return (Integer.toString(user_id)+"   "+first_name+"   "+last_name+
 						"   "+username+"   "+user_type+"   "+last_login_time);
@@ -159,11 +227,21 @@ public class MultFormatDataJoiner {
 
 	public static void main(String[] args) {
 		DataCompiler data = new DataCompiler();
+		
+		//false by default, if enabled compiled data will
+		//be sorted every time another chunk is added
+		//data.autoSort(true);
+		
 		data.add("Data/users.csv");
 		data.add("Data/users.json");
 		data.add("Data/users.xml");
+		
+		//not needed if autoSort is enabled
 		data.sort();
+		
+		//prints compiled data to console
 		data.printDataList();
+		
 		//folder name for output data
 		data.write("Result");
 	}
